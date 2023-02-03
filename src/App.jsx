5cd99f8ce11dfba2df4e30082091bec521a9fc76
krishnadev7 <krishnadevv12@gmail.com>
2023-02-03 22:50:47 +0530

    logo added in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { Feed } from '@mui/icons-material';
+import { Box } from '@mui/system';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ChannelDetail from './components/ChannelDetail';
+import Navbar from './components/Navbar';
+import SearchFeed from './components/SearchFeed';
+import VideoDetails from './components/VideoDetails';
 
-function App() {
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetails />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
-  return (
-    <div className="App">
-      hello world
-    </div>
-  )
-}
-
-export default App
+export default App;
