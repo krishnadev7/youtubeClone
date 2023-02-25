4cc8e571766dc1f5b2c1d67105b533c6f53ac4f4
krishnadev7 <krishnadevv12@gmail.com>
2023-02-25 20:07:02 +0530

    searchFeed added

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchFromApi } from '../utils/fetchFrom';
+import Videos from './Videos';
 
 const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${searchTerm}`).then(data =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
   return (
-    <div>SearchFeed</div>
-  )
-}
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      <Typography
+        fontWeight={'bold'}
+        variant='h4'
+        mb={2}
+        sx={{ color: 'white' }}
+      >
+        Search Results for{' '}
+        <span style={{ color: '#F31503' }}>{searchTerm} Videos</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
 
-export default SearchFeed
+export default SearchFeed;
